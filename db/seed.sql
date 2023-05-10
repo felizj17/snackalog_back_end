99aa0059a850b2ef2b01514b4717b62407c12f20
Felizj17 <felizj17@gmail.com>
2023-05-09 21:03:30 -0400

    changed link on seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 \c snacks_dev;
 
 INSERT INTO snacks (name, category, url, sugar, protein, fiber, is_vegan, is_vegetarian, is_glutenFree, is_favorite) VALUES
-('Oreo', 'Cookies', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=', 6.6, 0.7, 0.4, false, false, false, true),
+('Oreo', 'Cookies', 'https://s7d1.scene7.com/is/image/CENODS/10020-newscripts-oreos?$responsive$&wid=300&qlt=90,0&resMode=sharp2', 6.6, 0.7, 0.4, false, false, false, true),
 ('Pringles Original', 'Chips', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=', 1, 1, 1, true, true, false, true),
 ('Doritos Nacho Cheese', 'Chips', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=', 1, 2, 1, true, false, true, true), 
 ('Chex Mix', 'Chips', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=', 2.3, 4.2, 1.8, false, false, false, false),
